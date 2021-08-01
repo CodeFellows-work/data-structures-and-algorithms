8d9a827377a328c9acc4d5d0c203844adfa4850e
sunny-lee3 <sunnylee3712@gmail.com>
2021-07-31 17:32:43 -0700

    cant finish last test requirement

--- javascript/hashtables/__tests__/hashtable.test.js
+++ javascript/hashtables/__tests__/hashtable.test.js
@@ -9,12 +9,26 @@ describe('testing hashtable methods', () => {
   it('testing the add method', ()=> {
     let table = new HashTable(1);
     table.add('name', 'sunny');
-    console.log(table);
     expect(table.contains('sunny'));
   });
   it('testing the get method', () => {
     let table = new HashTable(200);
     table.add('name', 'sunny');
     expect(table.get('name')).toStrictEqual({"name": "sunny"});
+  });
+  it('testing hashtable repeatedWord', () => {
+    let table = new HashTable(100);
+    let testCase = 'asdasdasfsdfasdasdfasi i i youkkjhkjh kjhk';
+    expect(table.repeatedWord(testCase)).toStrictEqual('i');
+  });
+  it('testing lots of the same word', () => {
+    let table = new HashTable(50);
+    let testCase = 'a a a a a a a a a a b a a a a a a';
+    expect(table.repeatedWord(testCase)).toStrictEqual('a');
+  });
+  it('testing no repeatable words', () => {
+    let table = new HashTable(100);
+    let testCase = 'poop poo po';
+    expect(table.repeatedWord(testCase)).toStrictEqual('');
   });
 });
